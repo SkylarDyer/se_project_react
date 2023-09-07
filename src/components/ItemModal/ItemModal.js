1e50c334b2c7a0ba9f0318d3a5dfa55008ac52b5
skylarlol <skdyer26@yahoo.com>
2023-09-07 03:23:09 -0500

    fixed close button for item modal

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,13 +1,23 @@
+import "../ItemModal/ItemModal.css";
+
 const ItemModal = ({ selectedCard, onClose }) => {
   return (
     <div className={`modal`}>
       <div className="modal__content">
-        <button type="button" onClick={onClose}>
-          Close
-        </button>
-        <img src={selectedCard.link} alt="Add Item"/>
-        <div>{selectedCard.name}</div>
-        <div>Weather type: {selectedCard.weather}</div>
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__close-button-white"
+        ></button>
+        <img
+          src={selectedCard.link}
+          alt="Add Item"
+          className="modal__image-preview"
+        />
+        <div className="modal__item-name">{selectedCard.name}</div>
+        <div className="modal__weather-type">
+          Weather type: {selectedCard.weather}
+        </div>
       </div>
     </div>
   );
